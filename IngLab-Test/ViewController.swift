f1da657d1c15ccc64b055520b1edce46f17f755c
mewanchathuranga <mc_gamer@live.com>
2018-08-06 18:06:24 +0800

    Initial Commit

---
+++ IngLab-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IngLab-Test
+//
+//  Created by Mewan Chathuranga on 06/08/2018.
+//  Copyright © 2018 Mewan Chathuranga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
